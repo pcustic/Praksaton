01570433e82896aac97e5507403931337058c832
Porin <porin.custic@gmail.com>
2016-07-05 20:16:02 +0200

    Micanje nekih postavki

--- model/db.class.php
+++ model/db.class.php
@@ -14,7 +14,7 @@ class DB
 	    {
 	    	try
 	    	{
-		    	DB::$db = new PDO( "mysql: host=rp2.studenti.math.hr; dbname=custic; charset=utf8", 'student', 'pass.mysql' );
+		    	DB::$db = new PDO( "mysql: host=rp2.studenti.math.hr; dbname=IME_BAZE; charset=utf8", USERNAME, SIFRA );
 		    	DB::$db-> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		    }
 		    catch( PDOException $e ) { exit( 'PDO Error: ' . $e->getMessage() ); }
